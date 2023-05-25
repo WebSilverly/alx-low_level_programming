a9ad962de9519d605d308e552da05a321975980f
Victor <victoriousmba@gmail.com>
2023-05-25 03:29:58 -0700

    loop bug

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,10 +2,15 @@
 
 void positive_or_negative(int i)
 {
+
 if (i > 0)
 printf("%d is positive\n", i);
+
 else if (i< 0)
 printf("%d is negative\n", i);
+
 else
 printf("%d is zero\n", i);
+
+return (0);
 }
