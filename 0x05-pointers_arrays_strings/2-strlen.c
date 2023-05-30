fb87b4f21a824967b451b4bc9704fa8deffbb3de
Victor <victoriousmba@gmail.com>
2023-05-29 20:58:38 -0700

    trofat

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* int _strlen - a function that return the length of a string
+* _strlen - A function that returns the length of a string.
 * @s: string
+* Return: length
 */
 int _strlen(char *s)
 {
