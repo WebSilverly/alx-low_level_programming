ab6c9f3db77b9659c912afc55955384139255568
victor <victoriousmba@gmail.com>
2023-10-24 05:46:30 -0700

    requirement check

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,37 +2,32 @@
 
 
 /**
-
  * sum_listint - calculates the sum of all the data in a listint_t list
-
  * @head: first node in the linked list
-
  *
-
  * Return: resulting sum
-
  */
 
 int sum_listint(listint_t *head)
 
 {
 
- int sum = 0;
+int sum = 0;
 
- listint_t *temp = head;
-
-
- while (temp)
-
- {
-
- sum += temp->n;
-
- temp = temp->next;
-
- }
+listint_t *temp = head;
 
 
- return (sum);
+while (temp)
+
+{
+
+sum += temp->n;
+
+temp = temp->next;
+
+}
+
+
+return (sum);
 
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,61 +23,61 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 {
 
- unsigned int i;
+unsigned int i;
 
- listint_t *new;
+listint_t *new;
 
- listint_t *temp = *head;
-
-
- new = malloc(sizeof(listint_t));
-
- if (!new || !head)
-
- return (NULL);
+listint_t *temp = *head;
 
 
- new->n = n;
+new = malloc(sizeof(listint_t));
 
- new->next = NULL;
+if (!new || !head)
 
-
- if (idx == 0)
-
- {
-
- new->next = *head;
-
- *head = new;
-
- return (new);
-
- }
+return (NULL);
 
 
- for (i = 0; temp && i < idx; i++)
+new->n = n;
 
- {
-
- if (i == idx - 1)
-
- {
-
- new->next = temp->next;
-
- temp->next = new;
-
- return (new);
-
- }
-
- else
-
- temp = temp->next;
-
- }
+new->next = NULL;
 
 
- return (NULL);
+if (idx == 0)
+
+{
+
+new->next = *head;
+
+*head = new;
+
+return (new);
+
+}
+
+
+for (i = 0; temp && i < idx; i++)
+
+{
+
+if (i == idx - 1)
+
+{
+
+new->next = temp->next;
+
+temp->next = new;
+
+return (new);
+
+}
+
+else
+
+temp = temp->next;
+
+}
+
+
+return (NULL);
 
 }
